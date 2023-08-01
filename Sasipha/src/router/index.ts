0802df6e8a1a625b44dd52eb7160dd26f8bcd307
sasipha thonkhan <sasiphathonkhan@MacBook-Air-khxng-sasipha.local>
2023-08-01 11:54:41 +0700

    Start Lab3

---
+++ Sasipha/src/router/index.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventListView.vue'
+import AboutView from '../views/AboutView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'EventList',
+      component: EventListView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: () => import('../views/CategoryViwe.vue')
+    },
+    {
+    path: '/student',
+    name: 'student',
+    component: () => import('../views/StudentView.vue')}
+
+    
+  ]
+})
+
+export default router
