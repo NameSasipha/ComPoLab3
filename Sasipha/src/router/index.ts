b8d90c481e9266c578a041ba6087583204c26fe2
sasipha thonkhan <sasiphathonkhan@MacBook-Air-khxng-sasipha.local>
2023-08-02 19:57:23 +0700

    3.1

--- Sasipha/src/router/index.ts
+++ Sasipha/src/router/index.ts
@@ -3,7 +3,10 @@ import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
 import CategoryView from '../views/CategoryViwe.vue'
 import SudentView from '../views/StudentView.vue'
-import EventDetailView from '../views/EventDetailView.vue'
+import EventDetailView from '../views/event/EventDetailView.vue'
+import EventEditView from '@/views/event/EventEditView.vue'
+import EventRegisterView from '@/views/event/EventRegisterView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,7 +37,20 @@ const router = createRouter({
       name: 'event-detail',
       component: EventDetailView,
       props: true
+    },
+    {
+      path: '/event/:id/edit',
+      name: 'event-edit',
+      props: true,
+      component: EventEditView 
+    },
+    {
+      path: '/event/:id/register',
+      name: 'event-register',
+      props: true,
+      component: EventRegisterView 
     }
+
 
     
   ]
